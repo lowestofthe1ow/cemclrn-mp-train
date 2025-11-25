4cdb9d4296c32224b9c1f80b58e135cfc34341ef
Lorenz Bernard Marqueses <lorenz_marqueses@dlsu.edu.ph>
2025-11-25 10:56:50 +0800

    Fix error in .gitignore hiding modules/models directory

---
+++ modules/models/SigNetCNN.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+
+OUTPUT_CLASSES = 128
+
+
+def initialize_weights(m):
+    if isinstance(m, (nn.Conv2d, nn.Linear)):
+        # Paper seems to have used Xavier uniform initialization
+        nn.init.xavier_uniform_(m.weight)
+        # Biases are initialized to zero
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+
+
+class SigNetCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # We define the CNN directly based on Table 1 of the SigNet paper:
+        # https://arxiv.org/pdf/1707.02131
+
+        self.features = nn.Sequential(
+            # ------------------------------------------------------------------
+            # (1) Convolution 96 × 11 × 11 (stride = 1) + ReLU
+            nn.Conv2d(
+                in_channels=1, out_channels=96, kernel_size=11, stride=1, padding=0
+            ),
+            nn.ReLU(inplace=True),
+            # ------------------------------------------------------------------
+            # (2) Local Response Norm. (α = 10^-4, β = 0.75, k = 2, n = 5)
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            # ------------------------------------------------------------------
+            # (3) Pooling 96 × 3 × 3 (stride = 2)
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            # ------------------------------------------------------------------
+            # (4) Convolution 256 × 5 × 5 (stride = 1, pad = 2) + ReLU
+            nn.Conv2d(
+                in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2
+            ),
+            nn.ReLU(inplace=True),
+            # ------------------------------------------------------------------
+            # (5) Local Response Norm. (α = 10^-4, β = 0.75, k = 2, n = 5)
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            # ------------------------------------------------------------------
+            # (6) Pooling 256 × 3 × 3 (stride = 2) + Dropout (p = 0.3)
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Dropout(p=0.3),
+            # ------------------------------------------------------------------
+            # (7) Convolution 384 × 3 × 3 (stride = 1, pad = 1) + ReLU
+            nn.Conv2d(
+                in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1
+            ),
+            nn.ReLU(inplace=True),
+            # ------------------------------------------------------------------
+            # (8) Convolution 256 × 3 × 3 (stride = 1, pad = 1) + ReLU
+            nn.Conv2d(
+                in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1
+            ),
+            nn.ReLU(inplace=True),
+            # ------------------------------------------------------------------
+            # (9) Pooling 256 × 3 × 3 (stride = 2) + Dropout (p = 0.3) + Flatten
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Dropout(p=0.3),
+            nn.Flatten(),
+        )
+
+        self.classifier = nn.Sequential(
+            # ------------------------------------------------------------------
+            # (10) FC (1024) + ReLU + Dropout (p=0.5)
+            # 108800 = 17 * 25 * 256
+            nn.Linear(108800, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            # ------------------------------------------------------------------
+            # (11) FC (OUTPUT_CLASSES) + ReLU
+            nn.Linear(1024, OUTPUT_CLASSES),
+            nn.ReLU(inplace=True),
+        )
+
+        self.features.apply(initialize_weights)
+        self.classifier.apply(initialize_weights)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+if __name__ == "__main__":
+    # Call to determine the size of the FC layer in (10)
+    cnn = SigNetCNN()
+    dummy_input = torch.randn(32, 1, 155, 200)
+    with torch.no_grad():
+        _ = cnn(dummy_input)
+    print(cnn)
---
+++ modules/models/SigNetSiamese.py
@@ -0,0 +1,68 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from modules.models.SigNetCNN import SigNetCNN
+
+MARGIN = 1
+LEARNING_RATE = 1e-4
+WEIGHT_DECAY = 0.0005
+MOMENTUM = 0.9
+FUZZY = 1e-8
+GAMMA = 0.1
+SCHEDULER_MILESTONES = [14, 18]  # 70% and 90% of total epochs (20)
+
+
+# See https://github.com/HarshSulakhe/siamesenetworks-pytorch/blob/master/loss.py
+# Based on Eq. 1 of SigNet paper (https://arxiv.org/pdf/1707.02131)
+# Values for α and β are not mentioned, so we use 1
+def contrastive_loss(output1, output2, y):
+    euclidean_distance = F.pairwise_distance(output1, output2)
+    pos = (1 - y) * torch.pow(euclidean_distance, 2)
+    neg = (y) * torch.pow(torch.clamp(MARGIN - euclidean_distance, min=0.0), 2)
+    contrastive_loss = torch.mean(pos + neg)
+
+    return contrastive_loss, euclidean_distance
+
+
+class SigNetSiamese(pl.LightningModule):
+    def __init__(self, learning_rate=1e-4, margin=1.0):
+        super().__init__()
+
+        # "Branch" CNN of the Siamese network
+        self.cnn = SigNetCNN()
+
+    def forward(self, x1, x2):
+        y1 = self.cnn(x1)
+        y2 = self.cnn(x2)
+        return y1, y2
+
+    def training_step(self, batch, batch_idx):
+        x1, x2, y = batch
+        output1, output2 = self(x1, x2)
+        loss = contrastive_loss(output1, output2, y)[0]
+
+        # Log training loss
+        self.log(
+            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return loss
+
+    # We skip defining validation_step for now, as we train with a fixed number of epochs instead.
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.RMSprop(
+            self.cnn.parameters(),
+            lr=LEARNING_RATE,
+            weight_decay=WEIGHT_DECAY,
+            momentum=MOMENTUM,
+            eps=FUZZY,
+        )
+
+        # Paper did not specify milestones for LR scheduler, so we use conventional 70% / 90%
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer,
+            milestones=SCHEDULER_MILESTONES,
+            gamma=GAMMA,  # Divide by 10
+        )
+
+        return optimizer
